322bfd1102daca7bb5a0c831b1cf8c59ffde5c2b
Mateo Baravalle <mateo.baravalle@alu.frt.utn.edu.ar>
2023-02-03 19:52:17 -0300

    Menu General Finalizado

--- Aplicacion.cpp
+++ Aplicacion.cpp
@@ -2,39 +2,87 @@
 #include <stdio.h>
 #include "rlutil.h"
 
-void menu(int &op);
+void menu(int &op,int &yCord);
 
 main()
 {
 	
-	int opc;
+	
+	int opc=0,yCord=0;
 	
 	do
 	{
-		menu(opc);
+		menu(opc,yCord);
 		
-	}while(opc != 4);
+	}while(opc !=1);
 
 }
 
-void menu(int &op)
+void menu(int &op,int &yCord)
 {
-	//Aqui no usamos system("cls") ya que esta funcion no sirve en linux
-	rlutil::cls();
-	printf(" ____________________\n");
-	printf("/========MENU========\\\n");
-	printf("|                    |\n");
-	printf("|       Opcion1      |\n");
-	printf("|       Opcion       |\n");
-	printf("|       Opcion3      |\n");
-	printf("\\____________________/\n");
-	rlutil::locate(8,4);
-	printf("%c",175);
-	op = rlutil::getkey();
-	rlutil::locate(30,8);
-	printf("Opcion --> %d",op);	
-	getch();
+
+	rlutil::locate(1,1);
+	printf("  Subir(W)/Bajar(S)/Selec(Enter)\n");
+	printf("       _____________________\n");
+	printf("      /=======GymStats======\\\n");
+	printf("      |                     |\n");
+	printf("      |   Tipo de Usuario   |\n");
+	printf("      |                     |\n");
+	printf("      |        Coach        |\n");
+	printf("      |      Recepcion      |\n");
+	printf("      |        Admin        |\n");
+	printf("      |                     |\n");
+	printf("      |        Salir        |\n");
+	printf("      \\____________________/\n");
+
+// Ubicacion del Puntero
+	rlutil::locate(13,7+yCord);
+	printf("%c\n\n",175);
+	rlutil::locate(23,7+yCord);
+	printf("%c\n\n",174);
 	
+	rlutil::hidecursor();
+
+	int key = rlutil::getkey();
+	
+	switch(key)
+	{
+		case 119: //Arriba
+			
+			if(yCord!=0)
+				if(yCord>2)
+					yCord-=2;
+				else
+					yCord--;
+			
+			break;	
+		case 115: //Abajo
+			
+			if(yCord!=4)
+				if(yCord!=2)
+					yCord++;
+				else
+					yCord+=2;
+			
+			break;
+		case 1:
+			switch(yCord)
+			{
+				case 0:
+					//recepcion();
+					break;
+				case 1:
+					//administracion();
+					break;
+				case 2:
+					//entrenadores();
+					break;
+				case 4:
+					op=1;
+					break;
+			}
+			break;
+	}
 	
 	/* INGRESO MANUAL CON VALIDACION
 	rlutil::locate(30,8);

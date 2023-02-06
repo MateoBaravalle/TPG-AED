89b816aadaae3066bcbc365fc4d3c46877488f4c
Camila Monetti <camr.7.9.96@gmail.com>
2023-02-06 15:57:33 -0300

    Login falta probar con archivo real

--- Login.cpp
+++ Login.cpp
@@ -1,101 +1,91 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "rlutil.h"
+#include <string.h>
+#include <conio.h>
 
-void menu(int &op,int &yCord);
+struct User{
+	char user[10], pass[32], name[60];
+};
 
-main()
-{
-	
-	
-	int opc=0,yCord=0;
-	
-	do
-	{
-		menu(opc,yCord);
-		
-	}while(opc !=1);
-
+int CheckUser(struct User u){
+		int in=0;
+		FILE *archUsers;
+		archUsers = fopen("Users.dat","rb");
+		User currentU; 
+		if(archUsers==null){
+			printf("Error: no se encuentra el archivo de usuarios.");
+			in=1;
+		}
+		else{
+			do{
+				fread(&currentU, sizeof(u), 1, archUsers);
+				if(u.user==currentU.name && u.pass==currentU.pass) int=1;
+				else fseek(archUsers,sizeof(u),SEEK_CUR);
+			}while(feof(archUsers)==0)
+		}
+		return in;		
 }
 
-void menu(int &op,int &yCord)
-{
+void Login(int &in,int &yCord, struct User &u){
+	int xCord=22;
 
 	rlutil::locate(1,1);
 	printf("  Subir(W)/Bajar(S)/Selec(Enter)\n");
-	printf("       _____________________\n");
-	printf("      /=======GymStats======\\\n");
-	printf("      |                     |\n");
-	printf("      |   Tipo de Usuario   |\n");
-	printf("      |                     |\n");
-	printf("      |        Coach        |\n");
-	printf("      |      Recepcion      |\n");
-	printf("      |        Admin        |\n");
-	printf("      |                     |\n");
-	printf("      |        Salir        |\n");
-	printf("      \\____________________/\n");
+	printf("       _____________________________\n");
+	printf("      /===========GymStats==========\\\n");
+	printf("      |                             |\n");
+	printf("      |            Login            |\n");
+	printf("      |              ____________   |\n");
+	printf("      |  Usuario    |            |  |\n");
+	printf("      |              ¯¯¯¯¯¯¯¯¯¯¯¯   |\n");
+	printf("      |              ____________   |\n");
+	printf("      |  Contraseña |            |  |\n");
+	printf("      |              ¯¯¯¯¯¯¯¯¯¯¯¯   |\n");
+	printf("      |                             |\n");
+	printf("      |            Salir            |\n");
+	printf("      \\_____________________________/\n");
 
 // Ubicacion del Puntero
-	rlutil::locate(13,7+yCord);
-	printf("%c\n\n",175);
-	rlutil::locate(23,7+yCord);
-	printf("%c\n\n",174);
-	
+	rlutil::locate(xCord,7+yCord);
+	printf("%c\n\n",95);	
 	rlutil::hidecursor();
-
 	int key = rlutil::getkey();
 	
 	switch(key)
 	{
 		case 119: //Arriba
-			
-			if(yCord!=0)
-				if(yCord>2)
-					yCord-=2;
-				else
-					yCord--;
-			
+			if(yCord!=0) yCord=yCord-3;
 			break;	
 		case 115: //Abajo
-			
-			if(yCord!=4)
-				if(yCord!=2)
-					yCord++;
-				else
-					yCord+=2;
-			
+			if(yCord!=6) yCord=yCord+3;
 			break;
 		case 1:
 			switch(yCord)
 			{
 				case 0:
-					recepcion();
+					gets(u.user);
+					yCord=3;
 					break;
-				case 1:
-					administracion();
+				case 3:
+					gets(u.pass);
+					CheckUser(u);
 					break;
-				case 2:
-					entrenadores();
-					break;
-				case 4:
+				case 6:
 					op=1;
 					break;
 			}
 			break;
 	}
-	
-	/* INGRESO MANUAL CON VALIDACION
-	rlutil::locate(30,8);
-	printf("Opcion: ");	
-	if(scanf("%d",&op)!=1)
-	{
-		_flushall();
-		rlutil::locate(25,10);
-		printf("Entrada invalida!!");
-		rlutil::locate(18,11);
-		printf("Por favor ingrese un Numero valido %c",175);
-		op = 1;
-		getch();
-	}
-	*/
+}
+
+main(){
+	int in=0, i=0, yCord=0;
+	char caracter;
+	User u;
+	do{
+		in = Login(in,yCord,u);
+	}while(in !=1);
+	if(in==0) printf("\n\nUsuario o contraseña incorrectos.");
+	else Menu();
 }
